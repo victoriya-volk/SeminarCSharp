a740d045913a5e8f2e52ccfb60a65a2687d287b6
Victoriya Volk <victoriyavolk@icloud.com>
2022-09-27 12:02:31 +0300

    seminar 9

---
+++ seminar9.1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+int startNum = 1;
+void PrintNatural (int n)
+{
+    if (startNum <= n)
+    {
+        Console.Write($"{startNum}");
+        startNum++;
+        PrintNatural(n);
+    }
+}
+
+PrintNatural(5);
+Console.WriteLine();
+PrintNatural(15);
---
+++ seminar9.3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число 
+// и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int SumPartsNumber (int num)
+{
+    if (num / 10 == 0 ) return +num;
+    else
+    {
+        return SumPartsNumber(num/10) + num % 10;
+    }
+}
+
+Console.WriteLine(SumPartsNumber(453));
